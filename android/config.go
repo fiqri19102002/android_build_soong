83327656121a1c14419740e3b783919c8f1ac92f
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-12-24 02:08:10 +0000

    Revert "soong: Treat vendor/lineage-priv/* as release-keys"
    This reverts commit b9a45a53a330f09eb2124e9329726acf4e2e0492.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- android/config.go
+++ android/config.go
@@ -1376,9 +1376,6 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
-	if strings.HasPrefix(defaultCert, "vendor/lineage-priv/") {
-		return "release-keys"
-	}
 	return "dev-keys"
 }
 
